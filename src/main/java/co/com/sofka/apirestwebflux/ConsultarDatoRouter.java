078e1a57daab4475fb30268b928e716f1fb7bc58
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-14 23:00:22 -0500

    Agregando funcion router para el caso de uso consultar Dato

---
+++ src/main/java/co/com/sofka/apirestwebflux/ConsultarDatoRouter.java
@@ -0,0 +1,24 @@
+package co.com.sofka.apirestwebflux;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class ConsultarDatoRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> getAll(UseCaseListar useCaseListar) {
+        return route(GET("/consultar").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(useCaseListar.get(), DatoDTO.class)));
+    }
+}
