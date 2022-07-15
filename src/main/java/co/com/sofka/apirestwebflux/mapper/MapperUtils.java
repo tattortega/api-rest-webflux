62315f8134b0cb15af57183c43853672bf1f4b6b
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-14 23:14:24 -0500

    Agregando configuracion para el mapeo de entrada para los servicios

--- src/main/java/co/com/sofka/apirestwebflux/mapper/MapperUtils.java
+++ src/main/java/co/com/sofka/apirestwebflux/mapper/MapperUtils.java
@@ -1,7 +1,10 @@
 package co.com.sofka.apirestwebflux;
 
+import org.springframework.stereotype.Component;
+
 import java.util.function.Function;
 
+@Component
 public class MapperUtils {
 
     public Function<DatoDTO, Dato> mapperToDato(String id) {
