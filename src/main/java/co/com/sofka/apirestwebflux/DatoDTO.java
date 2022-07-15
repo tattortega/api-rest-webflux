087aaa537e91621f617bf9c37d4faaeae790328b
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-14 22:33:16 -0500

    Agregando DTO para el modelo Datos

---
+++ src/main/java/co/com/sofka/apirestwebflux/DatoDTO.java
@@ -0,0 +1,60 @@
+package co.com.sofka.apirestwebflux;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+public class DatoDTO {
+
+    private String id;
+    @NotBlank
+    private String informacion;
+
+    public DatoDTO() {
+    }
+
+    public DatoDTO(String informacion) {
+        this.informacion = informacion;
+    }
+
+    public DatoDTO(String id, String informacion) {
+        this.id = id;
+        this.informacion = informacion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DatoDTO datoDTO = (DatoDTO) o;
+        return Objects.equals(id, datoDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DatoDTO{" +
+                "id='" + id + '\'' +
+                ", informacion='" + informacion + '\'' +
+                '}';
+    }
+}
